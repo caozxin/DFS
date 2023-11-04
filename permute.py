84af9d319a9d92a5814e83a9ac67d657aed4a17a
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2023-11-04 11:36:19 -0400

    Add files via upload

--- permute.py
+++ permute.py
@@ -2,6 +2,7 @@ from typing import (
     List,
 )
 
+from collections import deque
 class Solution:
     """
     @param nums: A list of integers.
@@ -9,7 +10,6 @@ class Solution:
              we will sort your return value in output
 
     description: Given a list of numbers, return all possible permutations of it.
-
 
     """
     def permute(self, nums: List[int]) -> List[List[int]]:
@@ -29,7 +29,46 @@ class Solution:
         dfs(nums, [], used, res)
         return res
 
+    def permute02(self, nums: List[int]) -> List[List[int]]:
+        # write your code here
+        def baktrack(nums, permutation,  res):    # Nested helper function
+            if len(permutation) == len(nums):     
+                res.append(permutation)            # Add current permutation to result if it uses all numbers
+                return
+            
+            for i in range(len(nums)):
+
+                a_num = nums.pop(0) # we want to pop the first one
+                
+                permutation = baktrack(nums, permutation, res)
+                # permutation.append(new_solution)
+                nums.append(a_num)
+
+
+
+        res = []
+        baktrack(nums, [], res)
+        return res
+    
+    def permute03(self,nums: List[int]) -> List[List[int]]:
+        def backtrack(remaining_nums, current_permutation, result):
+            if not remaining_nums:
+                result.append(current_permutation[:])  # Append a copy of the current permutation
+                return
+
+            for i in range(len(remaining_nums)):
+                num = remaining_nums.pop(i)  # Remove the i-th element from the remaining numbers
+                current_permutation.append(num) #--> make the choice
+                backtrack(remaining_nums, current_permutation, result)
+                current_permutation.pop()  # Remove the last element (backtrack)
+                remaining_nums.insert(i, num)  # Restore the removed element with the position does not change --> undo the choice
+
+        result = []
+        backtrack(nums, [], result)
+        return result
+
+    
 new_solution = Solution()
 list = [1,2,3]
-result = new_solution.permute(list)
+result = new_solution.permute03(list)
 print("result", result)
