80de34ec6d9f56ab2f07a731b6d29ed7d6223747
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2023-12-27 09:17:40 -0500

    Update and rename subsets.py to Subsets.py with DFS

--- Subsets.py
+++ Subsets.py
@@ -104,7 +104,36 @@ class Solution:
         results = dfs(nums, 0, [], [])
         results.extend(result)
         return results
-    
+        
+    def subsets_withdfs(self, nums: List[int]) -> List[List[int]]:
+        if not nums: # None handeling
+            return [[]]
+
+        subset = []
+        index = 0
+        result = [] # we create a global variable here for the results. 
+
+        def dfs_backtracking(index): # best version so far
+            print("$$$index", index)
+            if index >= len(nums): # this is our recursion base case
+                print("***reach the base")
+                result.append(subset.copy()) # we need a deep copy here, since we are modifying subsets
+                return 
+
+            #decision to include nums[index]
+            print("subset to append", subset)
+            subset.append(nums[index])
+            dfs_backtracking(index + 1)
+
+            #decision to not to include nums[index]:
+            print("subset to pop", subset)
+            subset.pop()
+            dfs_backtracking(index + 1)
+        
+
+        dfs_backtracking(0) # we start with index 0 
+
+        return result
 
 
 
