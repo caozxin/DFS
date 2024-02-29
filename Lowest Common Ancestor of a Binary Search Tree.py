914e5f7243416805033d3bad6d1cbf0ba2ec1aae
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-02-28 21:46:18 -0500

    Create Lowest Common Ancestor of a Binary Search Tree.py

---
+++ Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,40 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: root of the tree
+    @param p: the node p
+    @param q: the node q
+    @return: find the LCA of p and q --> you need to return the parent node!
+    """
+    def lowestCommonAncestor_01(self, root, p, q): # this should work for binary tree search alone, but need to debug it!
+        # self.tree_traversal (root)
+        # note: for binary search tree, all left node is smaller than root and right node is bigger than root.
+
+        x = root.val
+        if p.val < x and q.val < x:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > x and q.val < x:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        return root
+
+    def lowestCommonAncestor(self, root, p, q): # this solution applies for LCA of a binary tree too
+        if root is None or root is p or root is q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
+
