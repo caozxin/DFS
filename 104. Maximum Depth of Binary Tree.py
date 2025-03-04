da29959a8fd2874057bf5f41ab1c8a13e3ee2aeb
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-04 04:04:52 -0500

    Update 104. Maximum Depth of Binary Tree.py

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -6,12 +6,12 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
+        # if the parent node <= child.node, child is visible!
         max_dep = [0] # this is a global variable and it needs to be mutal
 
         def dfs(node, depth):
             
-            if node is None: # here return values from child to parent. 
+            if node is None:
                 max_dep[0] = max(max_dep[0], depth) # max_dep needs to be updated here. also note that depth is local here. 
                 return  # once it reaches the end, it will just retun to previous level.
             
@@ -19,7 +19,7 @@ class Solution:
             
             # max_dep[0] += 1 --> you cannot count depth here, instead, count it in child level
             # print("max_dep", max_dep[0])
-            left = dfs(node.left, depth + 1) # depth + 1 is the state here, where we want to pass the depth from parent to child
+            left = dfs(node.left, depth + 1) # depth + 1 is the state here, where we want to pass the depth from parent to
             right = dfs(node.right, depth + 1)
 
         dfs(root, max_dep[0])
