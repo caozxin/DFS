d54f185109acc54d027b0b73631b2d50b460b558
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-05 07:19:11 -0500

    Update 572. Subtree of Another Tree.py

--- 572. Subtree of Another Tree.py
+++ 572. Subtree of Another Tree.py
@@ -29,3 +29,26 @@ class Solution:
             return dfs(node.left) or dfs(node.right)  # Check left and right subtrees
 
         return dfs(root)
+        
+    """ second version: 
+    """
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        def is_same_tree(tree1, tree2):
+            if tree1 is None and tree2 is None:
+                return True
+            if tree1 is None or tree2 is None:
+                return False
+            return (tree1.val == tree2.val and is_same_tree(tree1.left, tree2.left) and is_same_tree(tree1.right, tree2.right))
+
+
+        def dfs(root, subRoot):
+            """Traverse the tree to check if subRoot exists"""
+            if not root:
+                return False  # Base case: Empty tree can't contain subRoot
+
+            return is_same_tree(root, subRoot) or dfs(root.left, subRoot) or dfs(root.right, subRoot)
+
+
+        return dfs(root, subRoot)
