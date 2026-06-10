ce275ea3401289eb9ad3aba0d7c59d00ce800d80
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2026-06-10 18:03:39 -0400

    Update 226. Invert Binary Tree.py

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -97,3 +97,31 @@ class Solution:
             return node  # Return the inverted subtree
 
         return dfs(root)
+### update 06/10/26:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # self.val_map = list()
+        
+        def dfs(root):
+            if not root:
+                return
+            # print(root.val)
+            # self.val_map.append(root.val)
+
+            # if root.left and root.right:
+            root.left, root.right = root.right, root.left
+            # print(root.left, root.right)
+            # exit()
+            dfs(root.left)
+            dfs(root.right)
+
+        dfs(root)
+        # print(root)
+        return root
+        # print(self.val_map)
