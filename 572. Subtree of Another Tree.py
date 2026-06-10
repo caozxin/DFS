9fe014013114050f9fe4da0aaf17bdaba321a1a8
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2026-06-10 18:56:20 -0400

    Update 572. Subtree of Another Tree.py

--- 572. Subtree of Another Tree.py
+++ 572. Subtree of Another Tree.py
@@ -52,3 +52,39 @@ class Solution:
 
 
         return dfs(root, subRoot)
+
+
+### update 06/10/26:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # this question ask to list out all subtree, including root
+
+        def isSametree(p, q):
+
+            if not p and not q:
+                return True
+
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return isSametree(p.left, q.left) and isSametree(p.right, q.right)
+
+        def dfs(root, subRoot):
+            if not root:
+                return False
+            # print(root.val)
+            if isSametree(root, subRoot):
+                return True
+            
+            return dfs(root.left, subRoot) or dfs(root.right, subRoot)
+
+        return dfs(root, subRoot)
