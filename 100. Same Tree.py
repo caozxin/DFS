9583592c248db14c0a4f48170f680a8e1cda66e3
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2026-06-10 15:02:00 -0400

    Update 100. Same Tree.py

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -26,3 +26,22 @@ class Solution:
             return False
         else:
             return True
+
+
+### update 06/10/26 correction version: 
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        def dfs(p, q):
+            if not p and not q:
+                return True
+
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+
+        return dfs(p, q)
